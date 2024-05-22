fcfb13b7ef09bc12da1968c50ef123bb0a858ed9
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-05-22 12:52:13 -0300

    #HOT: Ajustando os nomes das constraints da tabela de funcionários

--- src/main/resources/db/migration/V2__create-table-funcionario.sql
+++ src/main/resources/db/migration/V2__create-table-funcionario.sql
@@ -7,12 +7,12 @@ CREATE TABLE funcionario (
     telefone VARCHAR(15) NOT NULL,
     tipo VARCHAR(20) NOT NULL,
     ativo TINYINT(1) NOT NULL,
-    CONSTRAINT pk_usuario_id
+    CONSTRAINT pk_funcionario_id
         PRIMARY KEY (id),
-    CONSTRAINT unq_usuario_login
+    CONSTRAINT unq_funcionario_login
         UNIQUE (login),
-    CONSTRAINT unq_usuario_email
+    CONSTRAINT unq_funcionario_email
         UNIQUE (email),
-    CONSTRAINT unq_usuario_telefone
+    CONSTRAINT unq_funcionario_telefone
         UNIQUE (telefone)
 )ENGINE=INNODB;
