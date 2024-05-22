8ee9976ee774d30508cbadc104f8f6660d545827
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-05-22 08:48:01 -0300

    #FEAT: Removendo imports inúteis

--- src/main/java/com/chillvery/api/repository/EmpresaRepository.java
+++ src/main/java/com/chillvery/api/repository/EmpresaRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Arrays;
-import java.util.List;
-
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
     Page<Empresa> findAllByAtivoTrue(Pageable paginacao);
 }
